ff23494c3966220c178b79d3e4643bb7f5c93859
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-03-31 20:29:15 -0300

    se puede escribir y cambiar la info del contacto, falta activar boton para guardar los cambios

--- exercise-contact-list-context/src/js/views/EditContact.js
+++ exercise-contact-list-context/src/js/views/EditContact.js
@@ -21,7 +21,20 @@ export const EditContact = props => {
 			})
 			setContact(contact[0]); //le paso el parametro nuevo con los obj q coincidan con el id
 		}
-	});
+	}, []);
+
+const handleChange = e => {
+	const {name , value}  = e.target;
+	setContact({ ...contact, [name]: value});
+};
+
+	const handleChange2 = e => {
+		const { name, value } = e.target;
+		const newcontact = Object.assign({}, contact);
+		newcontact[name] = value;
+		setContact({...newcontact}); // si el atributo existe dentro de contacto lo cambia por el valor q estoy obteniendo
+	};
+
 	return (
 		<div className="container">
 			<div>
@@ -29,19 +42,27 @@ export const EditContact = props => {
 				<form>
 					<div className="form-group">
 						<label>Full Name</label>
-						<input type="text" className="form-control" placeholder="Full Name" value={contact.full_name} />
+						<input type="text"
+							name="full_name"
+							className="form-control"
+							placeholder="Full Name"
+							value={contact.full_name}
+							onChange={handleChange} />
 					</div>
 					<div className="form-group">
 						<label>Email</label>
-						<input type="email" className="form-control" placeholder="Enter email" value={contact.email} />
+						<input type="email" name="email" className="form-control" placeholder="Enter email" value={contact.email}
+							onChange={handleChange} />
 					</div>
 					<div className="form-group">
 						<label>Phone</label>
-						<input type="phone" className="form-control" placeholder="Enter phone" value={contact.phone} />
+						<input type="phone" name="phone" className="form-control" placeholder="Enter phone" value={contact.phone}
+							onChange={handleChange} />
 					</div>
 					<div className="form-group">
 						<label>Address</label>
-						<input type="text" className="form-control" placeholder="Enter address" value={contact.address} />
+						<input type="text" name="address" className="form-control" placeholder="Enter address" value={contact.address}
+							onChange={handleChange} />
 					</div>
 					<button type="button" className="btn btn-success form-control">
 						save
